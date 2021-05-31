46b9d6f4455700df2c9e4ca6c0cef67e26c04c20
Leidy J. Rios Santofimio <45767724+LeYoarq@users.noreply.github.com>
2021-05-31 09:57:06 +0200

    CAMBIOS EN LA MAÑANA

---
+++ cmv_privado/JS/31-Mayo/31mayo-array.js
@@ -0,0 +1,10 @@
+//Ejercicios Arrays
+
+// Pasos:
+// Crear el Array.
+// Generar 7 elementos de forma aleatoria entre 1 y 7
+// Pedir por pantalla el metodo de ordenamiento:
+// 1. Mayor a menor.
+// 2. menor a mayor 
+// mostrar el array ordenado.
+
