273a03a6d90a43ecf9bc340e39ce50c3de1b4e5e
Leidy J. Rios Santofimio <45767724+LeYoarq@users.noreply.github.com>
2021-06-04 18:36:22 +0200

    Imágenes en galería
    Se agrega todas las imagenes

--- cmv_privado/JS/3-Junio/orla.js
+++ cmv_privado/JS/3-Junio/orla.js
@@ -1,5 +1,5 @@
 let registrados = [];
-var musica = []; //Array con mis elementos
+var music = []; //Array con mis elementos
 
 function readXML() {
     // lee desde GitHub.
@@ -17,92 +17,87 @@ function cargarArray(xml) {
     var i;
     var musNom;
     var musPie;
-    var nombre = [];
+    var elemento = [];
     var xmlDoc = xml.responseXML;
 
-
     var x = xmlDoc.getElementsByTagName("elemento");
-    // obtenemos algo así como x=[{USR1},{USR2}...,{CANDIDO}]
-
-    // tabla es una variable string que contiene codigo
     // html para poder mostrar en pantalla el XML con formato tabla
-
     let tabla = "<table><tr><th>Bandas</th><th>Fotos Iconicas</th></tr>";
     for (i = 0; i < x.length; i++) {
         // leo las etiquetas que me interesan del objeto
         let musNom = x[i].getElementsByTagName("nombre")[0].childNodes[0].nodeValue;
         let musPie = x[i].getElementsByTagName("foto")[0].childNodes[0].nodeValue; //Puse foto 
         // actualizo la tabla de visualización
-        tabla += "<tr><td>" + musNom + "</td><td>" + "<div> <img src='" + foto + "' /></td></tr>";
+        tabla += "<tr><td>" + musNom + "</td><td>" + "<div class=\"imgOrla\"><img class=\'imgOrla\'src=\"" + musPie + "\"/></div>" + "</td></tr>";
         // actualizo el array bidimensional con los usuarios registrados
-        nombre= [musNom, musPie];
-        musica.push(nombre);
-        
+        elemento = [musNom, musPie];
+        music.push(elemento);
     }
     tabla += "</table>"
-    document.getElementById("mensaje").innerHTML = tabla;
-
+    document.getElementById("mesage").innerHTML = tabla;
     // muestro en consola el array de orla
-  
-    musica.forEach((nombre) => {
-        elemento.forEach((elemento) => {
-        console.log(datos);
-         });
+    music.forEach((elemento) => {
+        elemento.forEach((datos) => {
+            console.log(datos);
+        });
     });
-
-}
-
-function MostrarClave() {
-    // obtenemos el usuario del campo input y
-    // validamos que exista en el array
-    // si no, mostramos un alert y no hacemos nada
-    // si existe actualizamos el elemento con id clavebuscada
-
-    // sintaxis jQuery: nombre = $("#nom").val();
-
-}
-
-
-function ascNombre() {
-    // Ordeno la matriz global
-    // acendente por nombre
-    registrados.sort(
-        (usuario1, usuario2) =>
-            usuario1[0].localeCompare(usuario2[0]),
-    );
-    mostrar();
 }
 
 
 
-function desClave() {
-    // Ordeno la matriz global
-    // descendente por clave
-    registrados.sort(
-        (usuario2, usuario1) =>
-            usuario1[1].localeCompare(usuario2[1]),
-    );
-    mostrar();
-}
+
+function showMusic() {
+    let bloque = "<section id=\"miOrla\" class='flex'>" ;// bloque de imagenes
+        for (i = 0; i < music.length; i++) {
+            let elemTit = music[i][0];
+            let elemImg = music[i][1];
+            bloque +=
+                `<figure class="container">
+                    <img class="img" src="${elemImg}">
+                    <div class="text">
+                        <h3>${elemTit}</h3>
+                    </div>
+            </figure> `;
+        }
+        bloque += "</section>"
+        document.getElementById("mesage1").innerHTML = bloque;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 
 function showMusic() {
     // en formato tabla en el id solicitado
-    let bloque = "<section id=\"miOrla\">";// bloque de imagenes
-    registrados.forEach((elemento) => {
-        elemento.forEach((datos) => {
+    let tabla = "<section id=\"miOrla\">";// bloque de imagenes
+    orla.forEach((elemento) => {//cambie por musica
+       /* elemento.forEach((orla) => {
             tabla = "<table><tr><th>EMPLEADO</th><th>CLAVE</th></tr>";
-            for (i = 0; i < registrados.length; i++) {
-                // leo las etiquetas que me interesan del objeto
-                elemTit = registrados[i][0];
-                elemImg= registrados[i][1];
+            for (i = 0; i < registrados.length; i++) {*/
+                // leo las etiquetas que me interesan del objeto*/
+                //elemTit = orla[i][0];
+                //elemImg= orla[i][1];
                 // actualizo la tabla de visualización
-                boque += "<div class=\"divOrla\"><img class=\"imgOrla\" src=\" + elemImg + \"/></div>";
+                //tabla += "<div class=\"divOrla\"><img class=\"imgOrla\" src=\" + elemImg + \"/></div>";
                 // actualizo el array bidimensional con los usuarios registrados
-            }
-            tabla += "</section>"
-            showMusic();//no sé
-        });
-    });
+            //}
+
+            //showMusic();//no sé
+        //});
+    //});
+    //tabla += "</section>"
     // la mostramos en el html
-    document.getElementById("solicitado").innerHTML = tabla;
-}
+    //document.getElementById("mensaje").innerHTML = tabla;
+//}
+
