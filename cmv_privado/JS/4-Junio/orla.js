95323d4ff588d8ea3f81533b9dbc2d3fa2d4f1b3
Leidy J. Rios Santofimio <45767724+LeYoarq@users.noreply.github.com>
2021-06-13 18:11:47 +0200

    Cambios sobre galería

--- cmv_privado/JS/4-Junio/orla.js
+++ cmv_privado/JS/4-Junio/orla.js
@@ -13,37 +13,21 @@ function readXML() {
 }
 
 function cargarArray(xml) {
-    var i;
-    var musNom;
-    var musPie;
     var elemento = [];
     var xmlDoc = xml.responseXML;
 
     var x = xmlDoc.getElementsByTagName("elemento");
-    // html para poder mostrar en pantalla el XML con formato tabla
-    let tabla = "<table><tr><th>Bandas</th><th>Fotos Iconicas</th></tr>";
     for (i = 0; i < x.length; i++) {
-        // leo las etiquetas que me interesan del objeto
         let musNom = x[i].getElementsByTagName("nombre")[0].childNodes[0].nodeValue;
-        let musPie = x[i].getElementsByTagName("foto")[0].childNodes[0].nodeValue; //Puse foto 
-        // actualizo la tabla de visualización
-        tabla += "<tr><td>" + musNom + "</td><td>" + "<div class=\"imgOrla\"><img class=\'imgOrla\'src=\"" + musPie + "\"/></div>" + "</td></tr>";
-        // actualizo el array bidimensional con los usuarios registrados
+        let musPie = x[i].getElementsByTagName("foto")[0].childNodes[0].nodeValue;
+        
         elemento = [musNom, musPie];
         music.push(elemento);
     }
-    tabla += "</table>"
-    document.getElementById("mesage").innerHTML = tabla;
-    // muestro en consola el array de orla
-    music.forEach((elemento) => {
-        elemento.forEach((datos) => {
-            console.log(datos);
-        });
-    });
 }
 
 function showMusic() {
-    let bloque = "<section id=\"miOrla\" class='flex'>" ;// bloque de imagenes
+    let bloque = "<section id=\"miOrla\" class='flex'>" ;
         for (i = 0; i < music.length; i++) {
             let elemTit = music[i][0];
             let elemImg = music[i][1];
@@ -56,5 +40,5 @@ function showMusic() {
             </figure> `;
         }
         bloque += "</section>"
-        document.getElementById("mesage1").innerHTML = bloque;
+        document.getElementById("mesage").innerHTML = bloque;
     }
